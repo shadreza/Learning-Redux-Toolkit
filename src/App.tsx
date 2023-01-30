46fbe71d5e8baef8cfc9b82c2d364417d9feb773
shadreza <shadreza100@gmail.com>
2023-01-30 13:08:35 +0600

    The Redux-Toolkit has been implemented and been tested

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { Container } from '@mui/material';
+import { Button, Container, Typography } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, incrementByAmount } from './redux/counter';
+import { RootState } from './redux/store';
 
 function App() {
+
+  const { count } = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <Container sx={styles.container}>
-      Hello
+      <Typography variant='h1'>Store</Typography>
+      <Typography variant='h4'>Counter : {count}</Typography>
+      <Container style={{marginTop: 20, display: 'flex', justifyContent: 'space-evenly'}}>
+        <Button variant="contained" onClick={()=>dispatch(increment())}>Increment</Button>
+        <Button variant="outlined" onClick={()=>dispatch(decrement())}>Decrement</Button>
+        <Button variant="text" onClick={()=>dispatch(incrementByAmount(33))}>Increment by 33</Button>
+      </Container>
     </Container>
   );
 }
@@ -12,6 +25,8 @@ const styles = {
   container: {
     bgcolor: 'tomato',
     height: '100vh',
+    textAlign: 'center',
+    p: 1,
   },
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+// redux imports
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
